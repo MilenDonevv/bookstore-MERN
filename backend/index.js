aabf4b95a86240fc723b424555a18188cadd7d3a
MilenDonevv <milendonev95@gmail.com>
2024-09-08 18:48:19 +0300

    add MongoDB and mongoose to node js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 /*
-
+npm install -  express nodemon mongoose
 
 
 */
 
 
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -16,6 +17,20 @@ app.get('/', (request, response) => {
 
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`);
-});
+
+
+
+// connect to mongoDB database
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+
+        // this message should show up ONLY IF the db is connected to the express server
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
