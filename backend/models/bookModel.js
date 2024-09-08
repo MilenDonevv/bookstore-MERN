367f78e5477637362434cbb15addd84ff7b5f92f
MilenDonevv <milendonev95@gmail.com>
2024-09-08 18:57:38 +0300

    create book model w/ mongoose

---
+++ backend/models/bookModel.js
@@ -0,0 +1,27 @@
+// https://mongoosejs.com/
+// const Book = mongoose.model('Book', { name: String });  - create a Schema outside and use it here : { name: String }
+
+import mongoose, { mongo } from 'mongoose';
+
+
+
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            require: true
+        },
+
+        author: {
+            type: String,
+            required: true
+        },
+        publishYear: {
+            type: Number,
+            required: true
+        },
+        // ... other fields
+    }
+)
+
+export const Book = mongoose.model('Book', bookSchema);
