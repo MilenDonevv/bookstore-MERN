d3c49283b8bffbfe3f9614a296c8843036e1b552
MilenDonevv <milendonev95@gmail.com>
2024-09-11 14:39:35 +0300

    add env to backend

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,15 @@ npm install
 */
 
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import  BooksRoute  from "./Routes/booksRoute.js";
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+
+
+dotenv.config();
+const PORT = process.env.PORT || 5555;
 const app = express();
 
 // Middleware for parsing request body
@@ -46,7 +50,7 @@ app.use("/books", BooksRoute);
 
 // connect to mongoDB database
 mongoose
-  .connect(mongoDBURL)
+  .connect(process.env.mongoDBURL)
   .then(() => {
     console.log("App connected to database");
 
